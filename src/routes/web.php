712aac96797a48de91945ecd517553c333b2f4c4
okuyama wataru <startkyk@icloud.com>
2026-06-22 19:47:02 +0900

    Create customer delete feature

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,9 +7,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 一覧表示
 Route::get('/customers', [CustomerController::class, 'index']);
+
+// 新規登録
 Route::get('/customers/create', [CustomerController::class, 'create']);
 Route::post('/customers', [CustomerController::class, 'store']);
 
+// 編集
 Route::get('/customers/{id}/edit', [CustomerController::class, 'edit']);
 Route::put('/customers/{id}', [CustomerController::class, 'update']);
+
+// 削除
+Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
