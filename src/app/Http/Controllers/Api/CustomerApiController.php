be87589a5f07ebef9a0e5606d7fa74a4ca0677ec
okuyama wataru <startkyk@icloud.com>
2026-06-27 16:28:44 +0900

    Add customer CRUD REST API

--- src/app/Http/Controllers/Api/CustomerApiController.php
+++ src/app/Http/Controllers/Api/CustomerApiController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
+use Illuminate\Support\Facades\Log;
 
 class CustomerApiController extends Controller
 {
@@ -23,5 +26,65 @@ class CustomerApiController extends Controller
     public function show(Customer $customer)
     {
         return response()->json($customer, 200);
+    }
+
+    /**
+     * 顧客登録
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        $customer = Customer::create(
+            $request->validated()
+        );
+
+        Log::info('API Customer Created', [
+            'customer_id' => $customer->id,
+            'customer_name' => $customer->name,
+        ]);
+
+        return response()->json([
+            'message' => 'Customer created successfully',
+            'data' => $customer
+        ], 201);
+    }
+
+    /**
+     * 顧客更新
+     */
+    public function update(
+        UpdateCustomerRequest $request,
+        Customer $customer
+    )
+    {
+        $customer->update(
+            $request->validated()
+        );
+
+        Log::info('API Customer Updated', [
+            'customer_id' => $customer->id,
+            'customer_name' => $customer->name,
+        ]);
+
+        return response()->json([
+            'message' => 'Customer updated successfully',
+            'data' => $customer
+        ], 200);
+    }
+
+    /**
+     * 顧客削除
+     */
+    public function destroy(Customer $customer)
+    {
+        Log::info('API Customer Deleted', [
+            'customer_id' => $customer->id,
+            'customer_name' => $customer->name,
+        ]);
+
+        $customer->delete();
+
+        return response()->json([
+            'message' => 'Customer deleted successfully'
+        ], 200);
     }
 }
