c02b0af9f8dbc586519a062753e2c3a3b43f4e8b
okuyama wataru <startkyk@icloud.com>
2026-06-27 13:17:09 +0900

    Add customer GET API

--- src/app/Http/Controllers/Api/CustomerApiController.php
+++ src/app/Http/Controllers/Api/CustomerApiController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CustomerApiController extends Controller
 {
-    //
+    /**
+     * 顧客一覧取得
+     */
+    public function index()
+    {
+        $customers = Customer::orderBy('id', 'desc')->get();
+
+        return response()->json($customers, 200);
+    }
+
+    /**
+     * 顧客詳細取得
+     */
+    public function show(Customer $customer)
+    {
+        return response()->json($customer, 200);
+    }
 }
