34a256ed46972596404e4b268a3991e0060996e9
okuyama wataru <startkyk@icloud.com>
2026-06-27 09:28:10 +0900

    Refactor validation using Form Request

--- src/resources/views/customers/create.blade.php
+++ src/resources/views/customers/create.blade.php
@@ -7,34 +7,44 @@
 
 <h1>顧客登録</h1>
 
+@if ($errors->any())
+    <div style="color: red; margin-bottom: 20px;">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 <form action="/customers" method="POST">
 
     @csrf
 
     <div>
         <label>名前</label>
-        <input type="text" name="name">
+        <input type="text" name="name" value="{{ old('name') }}">
     </div>
 
     <br>
 
     <div>
         <label>メール</label>
-        <input type="email" name="email">
+        <input type="text" name="email" value="{{ old('email') }}">
     </div>
 
     <br>
 
     <div>
         <label>電話番号</label>
-        <input type="text" name="phone">
+        <input type="text" name="phone" value="{{ old('phone') }}">
     </div>
 
     <br>
 
     <div>
         <label>会社名</label>
-        <input type="text" name="company">
+        <input type="text" name="company" value="{{ old('company') }}">
     </div>
 
     <br>
