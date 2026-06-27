55a9387bd9d85b496e26a8c13eb3e754f2e2fa24
okuyama wataru <startkyk@icloud.com>
2026-06-27 22:00:46 +0900

    Add Sanctum authentication API

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CustomerApiController;
+use App\Http\Controllers\Api\AuthController;
 
-/*
+// ログインAPI（認証不要）
+Route::post(
+    '/login',
+    [AuthController::class, 'login']
+);
+
+// 認証が必要なAPI
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/customers', [CustomerApiController::class, 'index']);
@@ -15,12 +22,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/customers/{customer}', [CustomerApiController::class, 'update']);
 
     Route::delete('/customers/{customer}', [CustomerApiController::class, 'destroy']);
-});
-*/
 
-// ↓ 一時的に認証なしでアクセスできるようにする
-Route::get('/customers', [CustomerApiController::class, 'index']);
-Route::get('/customers/{customer}', [CustomerApiController::class, 'show']);
-Route::post('/customers', [CustomerApiController::class, 'store']);
-Route::put('/customers/{customer}', [CustomerApiController::class, 'update']);
-Route::delete('/customers/{customer}', [CustomerApiController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
